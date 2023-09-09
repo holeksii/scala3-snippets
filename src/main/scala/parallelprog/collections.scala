176526e6d83769feb7c3cb244c20fc414acc039a
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2023-09-09 20:44:37 +0300

    Separate collections and operations

--- src/main/scala/parallelprog/collections.scala
+++ src/main/scala/parallelprog/collections.scala
@@ -1,38 +1,5 @@
 package parallelprog
 
-def mapArrSeg[A, B](
-    in: Array[A],
-    left: Int,
-    right: Int,
-    f: A => B,
-    out: Array[B]
-): Unit =
-  var i = left
-  while i < right do
-    out(i) = f(in(i))
-    i += 1
-
-def mapArrayPar[A, B](
-    in: Array[A],
-    f: A => B,
-    numOfTasks: Int,
-    out: Array[B]
-): Unit =
-  if numOfTasks == 1 then mapArrSeg(in, 0, in.length, f, out)
-  else
-    val treshold = in.length / numOfTasks
-
-    def mapFromLeftToRight(left: Int, right: Int): Unit =
-      if right - left < treshold then mapArrSeg(in, left, right, f, out)
-      else
-        val mid = left + (right - left) / 2
-        parallel(
-          mapFromLeftToRight(left, mid),
-          mapFromLeftToRight(mid, right)
-        )
-
-    mapFromLeftToRight(0, in.length)
-
 enum Tree[T]:
   case Leaf(a: Array[T])
   case Node(left: Tree[T], right: Tree[T])
