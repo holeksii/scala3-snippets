ea276ca4354845c88dbda08d6f264d7493cdd465
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2023-09-09 15:26:55 +0300

    Implement pure functional concurrent `Tree`

--- src/main/scala/parallelprog/collections.scala
+++ src/main/scala/parallelprog/collections.scala
@@ -18,15 +18,31 @@ def mapArrayPar[A, B](
     numOfTasks: Int,
     out: Array[B]
 ): Unit =
-  val treshold = in.length / numOfTasks
+  if numOfTasks == 1 then mapArrSeg(in, 0, in.length, f, out)
+  else
+    val treshold = in.length / numOfTasks
 
-  def mapFromLeftToRight(left: Int, right: Int): Unit =
-    if right - left < treshold then mapArrSeg(in, left, right, f, out)
-    else
-      val mid = left + (right - left) / 2
-      parallel(
-        mapFromLeftToRight(left, mid),
-        mapFromLeftToRight(mid, right)
-      )
+    def mapFromLeftToRight(left: Int, right: Int): Unit =
+      if right - left < treshold then mapArrSeg(in, left, right, f, out)
+      else
+        val mid = left + (right - left) / 2
+        parallel(
+          mapFromLeftToRight(left, mid),
+          mapFromLeftToRight(mid, right)
+        )
 
-  mapFromLeftToRight(0, in.length)
+    mapFromLeftToRight(0, in.length)
+
+enum Tree[T]:
+  case Leaf(a: Array[T])
+  case Node(left: Tree[T], right: Tree[T])
+
+  def mapPar[R: Manifest](f: T => R): Tree[R] =
+    this match
+      case Leaf(a) =>
+        val res = new Array[R](a.length)
+        mapArrayPar(a, f, 1, res)
+        Leaf(res)
+      case Node(l, r) =>
+        val (lRes, rRes) = parallel(l.mapPar(f), r.mapPar(f))
+        Node(lRes, rRes)
